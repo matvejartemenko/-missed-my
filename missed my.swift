398f8fc876f8a88aa7fcf8dbbfd1c7586d36b92c
Mike <152637381+matvejartemenko@users.noreply.github.com>
2024-01-09 23:44:01 +0530

    Create  missed my.swift

---
+++ missed my.swift
@@ -0,0 +1,7 @@
+print("Oops, that's not even in the ocean.")
+            } else {
+                print("You missed my battleship!")
+                board[row][col] = "X"
+            }
+        }
+    }
